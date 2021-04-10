40ec3322639b56f4eee98702e22eb2c7d700fefa
Frank Fisk <frank.fisk@gm.com>
2021-04-10 17:08:15 -0400

    final minus the comments.

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function compute()
 {
-    principal = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-	years = document.getElementById("years").value;
-    interest = principal * years * rate / 100;
-	d = new Date().getFullYear() + Number(years);
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+	var futureDate = new Date().getFullYear() + Number(years);
 	
-	resultDiv = document.getElementById("result");
-	resultDiv.innerHTML += "<br/>If you deposit " + principal.toString() + ",";
-	resultDiv.innerHTML += "<br/>at an interest rate of " + rate.toString() + ".";
-	resultDiv.innerHTML += "<br/>You will receive an amount of " + interest.toString() + ",";
-	resultDiv.innerHTML += "<br/>in the year " + d.toString();
-
-
+    if (principal == 0 || principal < 0 ) {
+		alert("The principal amount must be a number greater than zero");
+	} else {
+        resultDiv = document.getElementById("result");
+        resultDiv.innerHTML = "<br/><br/>If you deposit <mark>" + principal.toString() + "</mark>,";
+        resultDiv.innerHTML += "<br/>at an interest rate of <mark>" + rate.toString() + "</mark>.";
+        resultDiv.innerHTML += "<br/>You will receive an amount of <mark>" + interest.toString() + "</mark>,";
+        resultDiv.innerHTML += "<br/>in the year <mark>" + futureDate.toString() + "</mark>";
+	}
+	document.getElementById("principal").focus();
 }
-
 function updateSlider(slideAmount) {
-    sliderDiv = document.getElementById("sliderAmount");
+    var sliderDiv = document.getElementById("sliderAmount");
     sliderDiv.innerHTML = slideAmount;
     }       
