a24a739d7e23e4c4c76dfd1615d898499812646f
Frank Fisk <frank.fisk@gm.com>
2021-04-10 15:57:29 -0400

    updates to js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-     
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+	years = document.getElementById("years").value;
+    interest = principal * years * rate / 100;
+	d = new Date().getFullYear() + Number(years);
+	
+	resultDiv = document.getElementById("result");
+	resultDiv.innerHTML += "<br/>If you deposit " + principal.toString() + ",";
+	resultDiv.innerHTML += "<br/>at an interest rate of " + rate.toString() + ".";
+	resultDiv.innerHTML += "<br/>You will receive an amount of " + interest.toString() + ",";
+	resultDiv.innerHTML += "<br/>in the year " + d.toString();
+
+
 }
-        
+
+function updateSlider(slideAmount) {
+    sliderDiv = document.getElementById("sliderAmount");
+    sliderDiv.innerHTML = slideAmount;
+    }       
